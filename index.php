6c2de7d66b91c870bc9eff295956810befce4249
nlikhopoi <junetomato@yandex.ru>
2020-11-11 10:50:21 +0800

    changed jquery to axios

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
     <div id="result"></div>
 
 
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
     <script src="service.js"></script>
     <script src="main.js"></script>
 </body>
